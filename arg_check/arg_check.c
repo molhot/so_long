adb1e17fe849dba630e609d36c7bf626d405a3e7
Atushi Sakata <sakata19991214@gmail.com>
2023-01-20 23:45:41 +0900

    push

--- arg_check/arg_check.c
+++ arg_check/arg_check.c
@@ -6,7 +6,7 @@
 /*   By: satushi <sakata19991214@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 22:14:18 by satushi           #+#    #+#             */
-/*   Updated: 2023/01/19 23:19:13 by satushi          ###   ########.fr       */
+/*   Updated: 2023/01/20 23:45:03 by satushi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,16 @@ bool	arg_check(int argc, char **args)
 	size_t	s_l;
 
 	if (argc != 2)
+	{
+		ft_printf("your args is wrong\n");
 		return (false);
+	}
 	s_l = ft_strlen(args[1]);
 	if (args[1][s_l - 1] != 'r' || args[1][s_l - 2] != 'e' || \
 	args[1][s_l - 3] != 'b' || args[1][s_l - 4] != '.')
+	{
+		ft_printf("file is must end with .ber\n");
 		return (false);
+	}
 	return (true);
 }
